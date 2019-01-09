2e3cf5f24cae68e0e6b726319236a5e9e4a1d2e7
Michael P. Starkweather <mpstark@gmail.com>
2019-01-08 23:21:04 -0600

    add tooltip for passive skills, remove dots for passive skills that don't provide a tooltip

--- AbilityRealizer/Patches.cs
+++ AbilityRealizer/Patches.cs
@@ -1,11 +1,14 @@
 ﻿using BattleTech;
 using BattleTech.Data;
+using BattleTech.UI;
+using BattleTech.UI.Tooltips;
 using Harmony;
 using HBS;
 using HBS.Logging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using SVGImporter;
 
 namespace AbilityRealizer
 {
@@ -24,6 +27,34 @@ namespace AbilityRealizer
         public static void Prefix(Pilot __instance)
         {
             Main.TryUpdateAbilities(__instance.pilotDef);
+        }
+    }
+
+    [HarmonyPatch(typeof(SGBarracksSkillPip), "Initialize")]
+    public static class SGBarracksSkillPip_Initialize_Patch
+    {
+        public static void Postfix(SGBarracksSkillPip __instance, string type, int index, bool hasPassives, AbilityDef ability)
+        {
+            if (hasPassives)
+            {
+                var simGame = LazySingletonBehavior<UnityGameInstance>.Instance.Game.Simulation;
+                if (simGame != null)
+                {
+                    // get the abilities that are not primary
+                    var abilities = simGame.GetAbilityDefFromTree(type, index).Where(x => !x.IsPrimaryAbility).ToList();
+
+                    // gets the first ability that has a tooltip
+                    var passiveAbility = abilities.Find(x => !(string.IsNullOrEmpty(x.Description.Name) || string.IsNullOrEmpty(x.Description.Details)));
+
+                    // clear the dot on tooltip-less dots
+                    if (passiveAbility == null)
+                        Traverse.Create(__instance).Field("skillPassiveTraitDot").GetValue<SVGImage>().gameObject.SetActive(false);
+
+                    if (passiveAbility != null)
+                        Traverse.Create(__instance).Field("SkillTooltip").GetValue<HBSTooltip>()
+                            .SetDefaultStateData(TooltipUtilities.GetStateDataFromObject(passiveAbility.Description));
+                }
+            }
         }
     }
 
